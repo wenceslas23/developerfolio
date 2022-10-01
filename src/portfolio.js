eee8658f8d2731496fc26a7c2b2ca02d3185f4b4
wenceslas23 <wenceslasthamar@gmail.com>
2022-10-01 15:45:05 -0700

    attestation new

--- src/portfolio.js
+++ src/portfolio.js
@@ -201,7 +201,7 @@ const bigProjects = {
   subtitle: "Mes certifications, attestations ou récompenses. 🏆 ",
   projects: [
     {
-      image: require("./assets/images/ats1.jpg"),
+      image: require("./assets/images/ats.jpg"),
       projectName: "Participation au Sambas Professionnels",
       projectDesc: "Atelier Entrepreunariat: Comment passer du business à une entreprise et comment vivre tout de l'intérieur vers l'extérieur",
       footerLink: [
